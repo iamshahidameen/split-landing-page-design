dcfdfda9ce095c55141e7dbc6e2169da617d7989
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-10 21:19:17 +0500

    Added functionality by using simply addEventListener MouseENter function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello, World!");
+const leftSide = document.querySelector(".left");
+const rightSide = document.querySelector(".right");
+const  mainWrap = document.querySelector(".container");
+
+
+leftSide.addEventListener("mouseenter", () => {
+    mainWrap.classList.add("hover-left");
+    mainWrap.classList.remove("hover-right");
+})
+rightSide.addEventListener("mouseenter", () => {
+    mainWrap.classList.add("hover-right");
+    mainWrap.classList.remove("hover-left");
+})
